700dbb905b16abb9ebbd36bcc88eb19ef3c7823c
Duy Hoang <duyh@sixthgearstudios.com>
2018-10-30 17:55:56 +0700

    update soul change method

--- shiki_utils.py
+++ shiki_utils.py
@@ -3,7 +3,7 @@ import time
 import screen_processor, emu_manager
 
 # Souls check!
-def soul_change(shiki_img, soulset_img):
+def soul_change(shiki_img, soulset_img=None):
     screen_processor.abs_search("bottom_scroll.png", click=True)
     screen_processor.wait("shiki_icon.png", click=True)
     screen_processor.wait("tho.png")
@@ -13,12 +13,15 @@ def soul_change(shiki_img, soulset_img):
 
     screen_processor.wait("soul_change.png", click=True)
     screen_processor.wait("soul_save.png")
-    if screen_processor.abs_search(soulset_img)[0] == -1:
+    if soulset_img and screen_processor.abs_search(soulset_img)[0] == -1:
         #TODO un-hardcode this
         emu_manager.mouse_click(568, 205, sleep=1)
         emu_manager.mouse_click(745, 458)
         screen_processor.wait(soulset_img)
-
+    else:
+        emu_manager.mouse_click(568, 205, sleep=1)
+        emu_manager.mouse_click(745, 458)
+        time.sleep(2)
     # Back
     emu_manager.mouse_click(49, 31, sleep=0.5)
     emu_manager.mouse_click(49, 31, sleep=0.5)
