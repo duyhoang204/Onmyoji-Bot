6dbe0fdbea77c893f4bdd83bd53f6dcdd3b9af82
unknown <duyh.prg@gmail.com>
2018-11-22 23:12:38 +0700

    update pdq

--- pdq.py
+++ pdq.py
@@ -20,46 +20,49 @@ class PDQ(BaseTask):
         while screen_processor.abs_search("pdq_invite.png")[0] == -1:
             time.sleep(2)
 
-        screen_processor.abs_search("bottom_scroll.png", precision=0.9, click=True)
+        begin = time.time()
+        screen_processor.abs_search("bottom_scroll.png", (1000, 550, WINDOW_WIDTH, WINDOW_HEIGHT), precision=0.9, click=True)
         # Click on Guild icon in main screen
         emu_manager.mouse_click(408, 628)
         screen_processor.wait("pdq_icon.png", precision=0.9, click=True)
         screen_processor.wait("pdq_target_guild.png", precision=0.94, click=True)
         screen_processor.wait("pdq_enemy_house.png")
         logger.info("Got to PDQ main screen")
-        for i in range(0, 1):
-            while screen_processor.abs_search("pdq_battle_lb_icon.png", precision=0.94)[0] == -1:
-                # Click Fight!
-                emu_manager.mouse_click(WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100)
-                time.sleep(2)
-            logger.info("Started battle phase")
-            # We got into battle!
-            screen_processor.wait("realm_back_btn.png")
-            select_team(1)
-            time.sleep(1)
-            emu_manager.mouse_click(*BATTLE_START_BTN)
-            screen_processor.wait("auto_icon.png")
-            while screen_processor.abs_search("auto_icon.png")[0] != -1:
-                time.sleep(2)
 
-            won = 0
-            while screen_processor.abs_search("pdq_complete.png", precision=0.93)[0] == -1:
-                if screen_processor.abs_search("pdq_enemy_house.png")[0] != -1:
-                    logger.info("We lost :(")
-                    break
-                if screen_processor.abs_search("battle_reward.png")[0] != -1:
-                    won += 1
-                    logger.info("Win: {}".format(won))
-                    screen_processor.wait_disappear("battle_reward.png", click=True)
-                    time.sleep(2)
-                    if screen_processor.abs_search("pdq_complete.png", precision=0.93)[0] != -1:
-                        break
+        while screen_processor.abs_search("pdq_battle_lb_icon.png", precision=0.94)[0] == -1:
+            # Click Fight!
+            emu_manager.mouse_click(WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100)
+            time.sleep(2)
+        logger.info("Started battle phase")
+        # We got into battle!
+        screen_processor.wait("realm_back_btn.png")
+        select_team(1)
+        time.sleep(1)
+        emu_manager.mouse_click(*BATTLE_START_BTN)
+        screen_processor.wait("auto_icon.png")
+        while screen_processor.abs_search("auto_icon.png")[0] != -1:
+            time.sleep(2)
 
-                    # Keep battling
-                    emu_manager.mouse_click(*BATTLE_START_BTN)
-                    screen_processor.wait("auto_icon.png")
-                    while screen_processor.abs_search("auto_icon.png")[0] != -1:
-                        time.sleep(2)
+        won = 0
+        while screen_processor.abs_search("pdq_complete.png", precision=0.93)[0] == -1 or time.time() - begin < 30 * 60:
+            # if screen_processor.abs_search("pdq_enemy_house.png")[0] != -1:
+            #     logger.info("We lost :(")
+            #     break
+            # if screen_processor.abs_search("battle_reward.png")[0] != -1:
+            #     won += 1
+            #     logger.info("Win: {}".format(won))
+            #     screen_processor.wait_disappear("battle_reward.png", click=True)
+            #     time.sleep(2)
+            #     if screen_processor.abs_search("pdq_complete.png", precision=0.93)[0] != -1:
+            #         break
+            #
+            #     # Keep battling
+            #     emu_manager.mouse_click(*BATTLE_START_BTN)
+            #     screen_processor.wait("auto_icon.png")
+            #     while screen_processor.abs_search("auto_icon.png")[0] != -1:
+            #         time.sleep(2)
+            emu_manager.mouse_click(*PDQ_TARGET)
+            time.sleep(2)
 
         logger.info("Finished pdq!")
         FINISHED_MAIN_LOOP[0] = True
